8a0cefb3479e616748d2b4544b9f6bd00dfba864
Pham Manh Hiep <phammanhhiep89@gmail.com>
2017-11-06 11:36:07 +0700

    Built and test successfully cky parse.

--- projects/parsing/cfg.py
+++ projects/parsing/cfg.py
@@ -1,64 +1,87 @@
+from collections import defaultdict
+
 def to_CNF (CFG):
 	dcount = 0 # dummy count
 	G = CFG['rules']
 	L = CFG['lexicon']
 	CNF = {
-		'rules': {},
+		'rules': defaultdict (lambda: [], {}),
 		'lexicon': L
 	}
 
 	for l,r in G.items ():
-		for ri in r:
-			i = r.index (ri)
-			if ri is None: continue
-			if len (ri.split (' ')) == 1: 
+		temp = [ri for ri in r]
+		while not _is_valid (CNF['rules'][l], L):
+			for ri in temp:
+				i = temp.index (ri)
 				_resolve_unit_production (ri, l, G, CNF)
-			elif len (ri.split (' ')) > 2:
-				if _is_mixed (ri, L):
-					dcount = _resolve_mixed (dcount, ri, i, l, G, L)
-				dcount = _resolve_more2_nonterminal (dcount, ri, i, l, G)
-		_clean_none (r, l, G)
+				dcount = _resolve_mixed (dcount, ri, i, l, CNF['rules'], G, L)
+				dcount = _resolve_more2_nonterminal (dcount, ri, i, l, G, CNF)
+			temp = [ri for ri in CNF['rules'][l]]
+	return CNF
 
-def _clean_none (r, l, G):
-	# remove None elements in the right side of a rule
-	# remove the rule if has not right side after remove None
-	newr = []
-	for ri in r:
-		if ri is not None: newr.append (ri)
-	G[l] = newr
+def _is_valid (r, L):
+	valid = True
+	if len (r) == 0: valid = False
+	for nt in r:
+		if _is_mixed (nt, L) or _has_more_2nt (nt) or _has_up (nt):
+			valid = False
+			break
+	return valid
 
 def _is_mixed (ri, L):
 	is_mixed = False
+	ri_split = ri.split (' ')
 	for k,v in L.items ():
 		for t in v:
-			if t in ri: 
-				is_mixed = True
-				return is_mixed
+			for rij in ri_split:
+				if t == rij:
+					is_mixed = True
+					return is_mixed
 	return is_mixed
 
-def _resolve_mixed (dcount, ri, i, l, G, L):
+def _has_more_2nt (ri):
+	hasmore2 = False
+	if len (ri.split (' ')) > 2: hasmore2 = True
+	return hasmore2
+
+def _has_up (ri):
+	hasup = False
+	if len (ri.split (' ')) == 1: hasup = True
+	return hasup
+
+def _resolve_mixed (dcount, ri, i, l, CNF_G, G, L):
 	# FIX: one possible issue if parse using the result of such function is that, the same terminal will have many different dummy non-terminals refer to. This may reduce the relation between the terminal with sourounding words.
+	if not _is_mixed (ri, L): 
+		if len (CNF_G[l]) == 0: CNF_G[l] = G[l] 
+		return dcount
 	ri_split = ri.split (' ')
+	CNF_G[l] = [i for i in G[l]]
 	for rij in ri_split:
 		if _is_mixed (rij, L):
 			dcount, dname = _gen_dummy_nonterminal (dcount)
 			L[dname] = [rij]
 			rij_index = ri_split.index (rij)
 			ri_split[rij_index] = dname
-			G[l][i] = ' '.join (ri_split)
+			ri_index = G[l].index (ri) 
+			CNF_G[l][ri_index] = ' '.join (ri_split)
 	return dcount
 
-def _resolve_more2_nonterminal (dcount, ri, i, l, G):
+def _resolve_more2_nonterminal (dcount, ri, i, l, G, CNF):
+	if not _has_more_2nt (ri): 
+		if len (CNF['rules'][l]) == 0: CNF['rules'][l] = G[l] 
+		return dcount
+	CNF['rules'][l] = [i for i in G[l]] if G.get (l, None) is not None and len (CNF['rules'][l]) == 0 else CNF['rules'][l]
 	dcount, dname = _gen_dummy_nonterminal (dcount)
 	ri_split = ri.split (' ')
 	dpart = ri_split[:-1]
 	rest = ri_split[-1]
 	sub_ri = ' '.join (dpart)
-	G[dname] = [sub_ri]
-	i = G[l].index (ri)
-	G[l][i] = '{} {}'.format (dname, rest)
+	CNF['rules'][dname] = [sub_ri]
+	i = CNF['rules'][l].index (ri)
+	CNF['rules'][l][i] = '{} {}'.format (dname, rest)
 	if len (dpart) > 2:
-		dcount = _resolve_more2_nonterminal (dcount, sub_ri, 0, dname, G)
+		dcount = _resolve_more2_nonterminal (dcount, sub_ri, 0, dname, G, CNF)
 	return dcount
 
 def _gen_dummy_nonterminal (dcount):
@@ -70,30 +93,27 @@ def _resolve_unit_production (ri, l, G, CNF):
 	# assume CFG is complete. No non-terminal, if not being mapped to a word in the lexicon, has no rule
 	# replace unit production with non-unit production. 
 	# one the way to iterate the chain of unit productions, fix any one found.
-	cur_r = ri
-	cur_l = l
-	while True:
-		if _is_terminal (cur_r, CNF['lexicon']):
-			cur_r_t = CNF['lexicon'][cur_r]
-			cur_l_t = CNF['lexicon'].get (cur_l, [])
-			cur_l_t = list (set (cur_r_t + cur_l_t))
-			CNF['lexicon'][cur_l] = cur_l_t
-			i = G[cur_l].index (cur_r)
-			G[cur_l][i] = None
-			break
-		else:
-			if _has_unit_production (cur_r, G):
-				for nt in G[cur_r]:
-					if nt is not None and len (nt.split (' ')) == 1:
-						_resolve_unit_production (nt, cur_r, G, CNF)
- 
-			i = G[cur_l].index (cur_r)
-			G[cur_l][i] = None
-			cur_l_nt = G[cur_l]
-			for nt in G[cur_r]:
-				if nt not in cur_l_nt and nt is not None:
-					G[cur_l].append (nt)
-			break
+	if not _has_up (ri): 
+		if len (CNF['rules'][l]) == 0: CNF['rules'][l] = G[l]
+		return
+	CNF['rules'][ri] = [x for x in G.get (ri, [])]
+	for nt in G.get (ri, []):
+		newl = ri
+		newri = nt
+		_resolve_unit_production (newri, newl, G, CNF)
+
+	if _is_terminal (ri, CNF['lexicon']):
+		cur_r_t = CNF['lexicon'][ri]
+		cur_l_t = CNF['lexicon'].get (l, [])
+		cur_l_t = list (set (cur_r_t + cur_l_t))
+		CNF['lexicon'][l] = cur_l_t
+
+	CNF['rules'][l] = [x for x in G[l]] if len (CNF['rules'][l]) == 0 else CNF['rules'][l]
+	newnt = CNF['rules'][l] + CNF['rules'][ri]
+	newnt = list (set (newnt))
+	if ri in CNF['rules'][l]:
+		del CNF['rules'][l][CNF['rules'][l].index (ri)]
+	CNF['rules'][l] = [x for x in newnt if x is not ri]
 
 def _is_terminal (t, L):
 	# check if the right side is a terminal
--- projects/parsing/test/test_cfg.py
+++ projects/parsing/test/test_cfg.py
@@ -2,8 +2,8 @@ import os, sys
 sys.path.insert (0, os.getcwd ())
 import cfg
 
-# from collections import defaultdict
-# import random, math 
+from collections import defaultdict
+import random, math 
 import pytest
 
 @pytest.fixture
@@ -14,7 +14,7 @@ def CFG ():
 			'NP': ['Pronoun', 'Proper-Noun', 'Det Nominal'],
 			'Nominal': ['Noun', 'Nominal Noun', 'Nominal PP'],
 			'VP': ['Verb', 'Verb NP', 'Verb NP PP', 'Verb PP'],
-			'PP': ['Preposition PP']
+			'PP': ['Preposition NP']
 		},
 		'lexicon': {
 			'Det': ['that', 'this', 'the', 'a'],
@@ -42,33 +42,67 @@ def test__has_unit_production ():
 	has_up = cfg._has_unit_production ('VP', CFG['rules'])
 	assert has_up is False	
 
+# @pytest.mark.skip ()
 def test__resolve_unit_production_1 ():
 	# when the right-side non-terminal of UP derives a terminal
 	CFG = { # arbitrary
 		'rules': {
 			'S': ['Verb'],
-			'PP': ['Verb']
+			'PP': ['K', 'AB XY'],
+			'K': ['Z', 'AC XY']
 		},
 		'lexicon': {
 			'Verb': ['x', 'y'],
-			'PP': ['z', 'y']
+			'PP': ['z', 'y'],
+			'Z': ['a', 'b']
 		}
 	}
 
 	CNF = {
-		'rules': {},
+		'rules': defaultdict (lambda: [], {}),
 		'lexicon': CFG['lexicon']
 	}
 
 	cfg._resolve_unit_production ('Verb', 'S', CFG['rules'], CNF)
-	cfg._resolve_unit_production ('Verb', 'PP', CFG['rules'], CNF)
+	cfg._resolve_unit_production ('K', 'PP', CFG['rules'], CNF)
 
 	assert len (CNF['lexicon']['S']) == 2
-	for i in ['x', 'y']:
-		assert i in CNF['lexicon']['S']
-	assert len (CNF['lexicon']['PP']) == 3
-	for i in ['x', 'y', 'z']:
-		assert i in CNF['lexicon']['PP']	
+	for i in ['x', 'y']: assert i in CNF['lexicon']['S']
+	assert len (CNF['rules']['S']) == 0
+
+	assert len (CNF['lexicon']['PP']) == 4
+	for i in ['y', 'z', 'a', 'b']: assert i in CNF['lexicon']['PP']
+	assert len (CNF['rules']['PP']) == 2
+	for i in ['AB XY', 'AC XY']: assert i in CNF['rules']['PP']
+
+# @pytest.mark.skip ()
+def test__resolve_unit_production_1_1 ():
+	# when the right-side non-terminal of UP derives a terminal. Special case: all non-terminal in right side derive terminals
+	CFG = { # arbitrary
+		'rules': {
+			'PP': ['K', 'AB XY'],
+			'K': ['Z', 'T'],
+		},
+		'lexicon': {
+			'PP': ['z', 'y'],
+			'Z': ['a', 'b'],
+			'T': ['n', 'm'],
+			'X': ['y', 'x'],
+			'Y': ['t', 'k'],
+		}
+	}
+
+	CNF = {
+		'rules': defaultdict (lambda: [], {}),
+		'lexicon': CFG['lexicon']
+	}
+
+	cfg._resolve_unit_production ('K', 'PP', CFG['rules'], CNF)
+
+	assert len (CNF['lexicon']['PP']) == 6
+	for i in ['y', 'z', 'a', 'b', 'n', 'm']: assert i in CNF['lexicon']['PP']
+	assert len (CNF['rules']['PP']) == 1
+	for i in ['AB XY']: assert i in CNF['rules']['PP']
 
 def test__resolve_unit_production_2 ():
 	# when the right-side non-terminal of UP derives which has no unit production
@@ -86,19 +120,17 @@ def test__resolve_unit_production_2 ():
 	}
 
 	CNF = {
-		'rules': {},
+		'rules': defaultdict (lambda: [], {}),
 		'lexicon': CFG['lexicon']
 	}
 
 	cfg._resolve_unit_production ('VP', 'S', CFG['rules'], CNF)
 	cfg._resolve_unit_production ('Z', 'K', CFG['rules'], CNF)
-	assert len (CFG['rules']['S']) == 4
-	assert None in CFG['rules']['S']
-	for t in ['Verb PP', 'Verb NP']: assert t in CFG['rules']['S']
+	assert len (CNF['rules']['S']) == 3
+	for t in ['Verb PP', 'Verb NP', 'VP PP']: assert t in CNF['rules']['S']
 
-	assert len (CFG['rules']['K']) == 3
-	assert None in CFG['rules']['K']
-	for t in ['Verb PP', 'VP PP']: assert t in CFG['rules']['K']
+	assert len (CNF['rules']['K']) == 2
+	for t in ['Verb PP', 'VP PP']: assert t in CNF['rules']['K']
 
 def test__resolve_unit_production_3 ():
 	# when the right-side non-terminal of UP derives one unit production 
@@ -106,8 +138,9 @@ def test__resolve_unit_production_3 ():
 		'rules': {
 			'S': ['VP', 'VP PP'],
 			'VP': ['K', 'Verb PP', 'Verb NP'],
-			'K': ['Z T', 'VP PP'], # arbitrary
-			'Z': ['Verb PP', 'VP PP'], # arbitrary
+			'K': ['Z T', 'VP PP', 'Z'], # arbitrary
+			'Z': ['N', 'VP PP'], # arbitrary
+			'N': ['XX XX', 'MM MM']
 		},
 		'lexicon': {
 			'Verb': ['x', 'y'],
@@ -116,14 +149,15 @@ def test__resolve_unit_production_3 ():
 	}
 
 	CNF = {
-		'rules': {},
+		'rules': defaultdict (lambda: [], {}),
 		'lexicon': CFG['lexicon']
 	}
 
 	cfg._resolve_unit_production ('VP', 'S', CFG['rules'], CNF)
-	assert len (CFG['rules']['S']) == 5
-	assert None in CFG['rules']['S']
-	for t in ['Verb PP', 'Verb NP', 'Z T', 'VP PP']: assert t in CFG['rules']['S'] 
+	for t in ['Verb PP', 'Verb NP', 'XX XX', 'MM MM', 'Z T', 'VP PP']: assert t in CNF['rules']['S']
+	for t in ['Verb PP', 'Verb NP', 'XX XX', 'MM MM', 'Z T', 'VP PP']: assert t in CNF['rules']['VP']
+	for t in ['XX XX', 'MM MM', 'Z T', 'VP PP']: assert t in CNF['rules']['K']
+	for t in ['VP PP', 'XX XX', 'MM MM']: assert t in CNF['rules']['Z']
 
 def test__resolve_unit_production_4 ():
 	# when UP derives non-terminal, which has more than one unit production
@@ -141,15 +175,14 @@ def test__resolve_unit_production_4 ():
 	}
 
 	CNF = {
-		'rules': {},
+		'rules': defaultdict (lambda: [], {}),
 		'lexicon': CFG['lexicon']
 	}
 
 	cfg._resolve_unit_production ('VP', 'S', CFG['rules'], CNF)
-	assert len (CFG['rules']['S']) == 5
-	assert None in CFG['rules']['S']
-	for t in ['Verb PP', 'Adverb Verb PP', 'Z T', 'VP PP']: assert t in CFG['rules']['S']
-	for t in ['Verb PP', 'Adverb Verb PP', 'Z T', 'VP PP']: assert t in CFG['rules']['VP']	
+	assert len (CNF['rules']['S']) == 4
+	for t in ['Verb PP', 'Adverb Verb PP', 'Z T', 'VP PP']: assert t in CNF['rules']['S']
+	for t in ['Verb PP', 'Adverb Verb PP', 'Z T', 'VP PP']: assert t in CNF['rules']['VP']
 
 def test__resolve_more2_nonterminal_ ():
 	# there are three or more nonterminals
@@ -157,19 +190,23 @@ def test__resolve_more2_nonterminal_ ():
 		'rules': {
 			'S': ['VP', 'VP PP XX'],
 			'NP': ['XX YY ZZ TT', 'NM IQ']
-		}
+		},
 	}
 
+	CNF = {
+		'rules': defaultdict (lambda: [], {}),
+	} 
+
 	d = 0
-	d = cfg._resolve_more2_nonterminal (d, 'VP PP XX', 1, 'S', CFG['rules'])
-	d = cfg._resolve_more2_nonterminal (d, 'XX YY ZZ TT', 0, 'NP', CFG['rules'])
+	d = cfg._resolve_more2_nonterminal (d, 'VP PP XX', 1, 'S', CFG['rules'], CNF)
+	d = cfg._resolve_more2_nonterminal (d, 'XX YY ZZ TT', 0, 'NP', CFG['rules'], CNF)
 
 	assert d == 3
-	assert CFG['rules']['S'][1] == 'Dummy_1 XX'
-	assert CFG['rules']['Dummy_1'][0] == 'VP PP'
-	assert CFG['rules']['NP'][0] == 'Dummy_2 TT'
-	assert CFG['rules']['Dummy_2'][0] == 'Dummy_3 ZZ'
-	assert CFG['rules']['Dummy_3'][0] == 'XX YY'
+	assert CNF['rules']['S'][1] == 'Dummy_1 XX'
+	assert CNF['rules']['Dummy_1'][0] == 'VP PP'
+	assert CNF['rules']['NP'][0] == 'Dummy_2 TT'
+	assert CNF['rules']['Dummy_2'][0] == 'Dummy_3 ZZ'
+	assert CNF['rules']['Dummy_3'][0] == 'XX YY'
 
 def test__is_mixed ():
 	ri1 = 'XX walk YY'
@@ -182,6 +219,7 @@ def test__is_mixed ():
 	assert cfg._is_mixed (ri1, L) is True
 	assert cfg._is_mixed (ri2, L) is False
 
+# @pytest.mark.skip ()
 def test__resolve_mixed ():
 	CFG = { # arbitrary
 		'rules': {
@@ -193,34 +231,27 @@ def test__resolve_mixed ():
 		}
 	}
 
+	CNF = {
+		'rules': defaultdict (lambda: [], {}),
+		'lexicon': CFG['lexicon']
+	} 
+
 	dcount = 0
-	dcount = cfg._resolve_mixed (dcount, 'Verb yy zz', 2, 'S', CFG['rules'], CFG['lexicon'])
+	dcount = cfg._resolve_mixed (dcount, 'Verb yy zz', 2, 'S', CNF['rules'], CFG['rules'], CNF['lexicon'])
 	assert dcount == 2
-	CFG['lexicon']['Dummy_1'][0] == 'yy'
-	CFG['lexicon']['Dummy_2'][0] == 'zz'
-	CFG['rules']['S'][2] = 'Verb Dummy_1 Dummy_2'
+	CNF['lexicon']['Dummy_1'][0] == 'yy'
+	CNF['lexicon']['Dummy_2'][0] == 'zz'
+	CNF['rules']['S'][2] = 'Verb Dummy_1 Dummy_2'
 
-def test__clean_none ():
-	G = {
-		'X': ['zz', None, 'yy'],
-		'Y': [None, None],
-		'Z': ['xx', 'aa']
-	}
-
-	cfg._clean_none (G['X'], 'X', G)
-	cfg._clean_none (G['Y'], 'Y', G)
-	cfg._clean_none (G['Z'], 'Z', G)
-
-	assert len (G['X']) == 2
-	assert None not in G['X']
-	assert len (G['Y']) == 0	  
-	assert len (G['Z']) == 2
-	assert None not in G['Z']
-
+# @pytest.mark.skip ()
 def test_to_CNF (CFG):
 	CNF = cfg.to_CNF (CFG)
+	assert len (CNF['rules']) == 12
 	assert len (CNF['rules']['S']) == 5
 	assert len (CNF['lexicon']['S']) == 3
-
-
+	for nt in CNF['rules']['S']: assert len (nt.split (' ')) == 2
+	for nt in CNF['rules']['VP']: assert len (nt.split (' ')) == 2
+	for nt in CNF['rules']['NP']: assert len (nt.split (' ')) == 2
+	for nt in CNF['rules']['Nominal']: assert len (nt.split (' ')) == 2
+	for nt in CNF['rules']['PP']: assert len (nt.split (' ')) == 2
 	
